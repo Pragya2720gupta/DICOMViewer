c409e982e68be0487a6716ff56308e4098ee8931
Pragya2720gupta <pragya2720gupta@gmail.com>
2024-12-26 12:02:42 +0530

    Update main.cpp
    Added comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,24 @@
-#include <QtWidgets/QApplication>
-#include <mainwindow.h>
+#include <QtWidgets/QApplication> // Include for the QWidgets from QApplication class
+#include <mainwindow.h>           // Include for the MainWindow class
 
+/**
+ * @brief The entry point of the application.
+ * 
+ * This function initializes the Qt application, creates the main window,
+ * and starts the event loop.
+ *
+ * @param argc The number of command-line arguments.
+ * @param argv The array of command-line arguments.
+ * @return int Returns the exit status of the application.
+ */
 int main(int argc, char* argv[]) {
-	QApplication app(argc, argv);
+    // Create a QApplication object
+    QApplication app(argc, argv);
 
-	MainWindow dicomViewer;
-	dicomViewer.show();
+    // Create and display the main window for the DICOM viewer
+    MainWindow dicomViewer;
+    dicomViewer.show();
 
-	return app.exec();
+    // Start the application's event loop
+    return app.exec();
 }
