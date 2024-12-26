c5c4c8ddc5fb1ff60fb0ee9c671bd09aff0c259b
Pragya2720gupta <pragya2720gupta@gmail.com>
2024-12-26 11:57:35 +0530

    Update mainwindow.h
    Removed unnecessary includes and added comments

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
+#include <QTimer>
+#include <vtkSmartPointer.h>
 #include <vtkGenericOpenGLRenderWindow.h>
 #include <vtkRenderer.h>
 #include <QVTKInteractor.h>
@@ -9,61 +12,77 @@
 #include <vtkImageViewer2.h>
 #include <vtkDICOMImageReader.h>
 #include <vtkSTLReader.h>
-#include <vtkNew.h>
-#include <vtkActor.h>
-#include <vtkImageActor.h>
-#include <vtkProperty.h>
-#include <vtkNamedColors.h>
 #include <vtkTransform.h>
-#include <vtkTransformPolyDataFilter.h>
-#include <vtkVolume.h>
-#include <vtkVolumeProperty.h>
-#include <vtkFixedPointVolumeRayCastMapper.h>
-#include <vtkColorTransferFunction.h>
-#include <vtkPiecewiseFunction.h>
 #include <vtkPolyDataMapper.h>
-#include "vtkSmartPointer.h"
-#include "vtkDICOMImageReader.h"
-#include "vtkImageData.h"
-#include "vtkImageViewer2.h"
-#include "vtkRenderer.h"
-#include "vtkRenderWindowInteractor.h"
-#include <QFileDialog>
-#include <iostream>
-#include<QMessageBox>
-#include <QTimer>
+#include <vtkActor.h>
+#include <vtkFixedPointVolumeRayCastMapper.h>
+#include <vtkVolume.h>
+#include <vtkNamedColors.h>
+#include <vtkTransformPolyDataFilter.h>
 
+// Forward declaration of the Ui namespace
 namespace Ui {
 class MainWindow;
 }
 
+/**
+ * @class MainWindow
+ * @brief The main application window for rendering DICOM images and STL files.
+ */
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructor
+     * @param parent Parent widget, default is nullptr.
+     */
     explicit MainWindow(QWidget *parent = nullptr);
+
+    /**
+     * @brief Destructor
+     */
     ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
-    int TestDICOMImageReader(std::string filename);
-    vtkSmartPointer<vtkGenericOpenGLRenderWindow> mRenderWindow;
-    vtkSmartPointer<vtkRenderer> mRenderer;
-    vtkSmartPointer<QVTKInteractor> mInteractor;
-    vtkSmartPointer<vtkInteractorStyle> mInteractorStyle;
-    vtkSmartPointer<vtkDICOMImageReader> mReader;
-    vtkSmartPointer<vtkImageViewer2> mViewer;
-    vtkSmartPointer<vtkTransform> mToolMover;
-    
-    bool mIncrementAngle;
-   
+    Ui::MainWindow *ui; ///< Pointer to the UI elements
+
+    // VTK components
+    vtkSmartPointer<vtkGenericOpenGLRenderWindow> mRenderWindow; ///< OpenGL render window
+    vtkSmartPointer<vtkRenderer> mRenderer;                     ///< VTK renderer
+    vtkSmartPointer<QVTKInteractor> mInteractor;                ///< VTK interactor
+    vtkSmartPointer<vtkInteractorStyle> mInteractorStyle;       ///< Interactor style
+    vtkSmartPointer<vtkDICOMImageReader> mReader;               ///< Reader for DICOM images
+    vtkSmartPointer<vtkImageViewer2> mViewer;                   ///< Viewer for images
+    vtkSmartPointer<vtkTransform> mToolMover;                   ///< Transform for tool movement
+
+    bool mIncrementAngle; ///< Flag to control the animation state
+
+    /**
+     * @brief Updates the tool's position during animation.
+     */
     void updateToolPosition();
+
+    /**
+     * @brief Starts the tool's movement animation.
+     */
     void startToolMovement();
-    
+
 public slots:
+    /**
+     * @brief Slot for handling the "Open DICOM File" button click.
+     */
     void onBtnOpenFileClick();
+
+    /**
+     * @brief Slot for handling the "Select STL Tool" button click.
+     */
     void onBtnSTLToolFile();
+
+    /**
+     * @brief Slot for handling the "Start/Stop" button click.
+     */
     void onBtnStartStopClicked();
 };
 
